c86479de098b0a2d4f5fcb09d6b2374934a5362a
mpetavy <petavy@gmx.net>
2019-10-21 21:13:30 +0200

    replace $host, $port, $address with values

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -jar target\discover-0.0.1-SNAPSHOT.jar --discover.port=9999
+java -jar target\discover-0.0.1-SNAPSHOT.jar --discover.port=15000
---
+++ run.sh
@@ -0,0 +1,2 @@
+mvn clean package
+java -jar target/discover-0.0.1-SNAPSHOT.jar --discover.port=15000 --discover.uid=hydra
